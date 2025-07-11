66b3c98c9cae51326e6e30c059c8eb540732ebf6
BlcakCobra <valeri.baskayanc.y@gmail.com>
2025-07-11 16:30:34 +0400

    modifyed App with new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import styles from "./App.module.scss"
 import Header from "./Components/Header/Header"
 import FirstBlock from "./Components/FirstBlock/FirstBlock"
+import Collaboration from "./Components/Collaboration/Collaboration"
+import Advantages from "./Components/Advantages/Advantages"
+// import Agenda from "./Components/Agenda/Agenda"
 
 const Home = () => (
-  
+
   <div className={styles.wrapper}>
     <Header />
     <FirstBlock />
+    <Collaboration />
+    <Advantages />
+    {/* <Agenda/> */}
   </div>
 )
 
