c1230fac24067560886ede10fcce2ab1edfd4ee7
BlcakCobra <valeri.baskayanc.y@gmail.com>
2025-07-14 18:52:16 +0400

    Adds new Component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,14 @@ import Header from "./Components/Header/Header"
 import FirstBlock from "./Components/FirstBlock/FirstBlock"
 import Collaboration from "./Components/Collaboration/Collaboration"
 import Advantages from "./Components/Advantages/Advantages"
-// import Agenda from "./Components/Agenda/Agenda"
+import Agenda from "./Components/Agenda/Agenda"
+// import KeynoteSpeakers from "./Components/KeynoteSpeakers/KeynoteSpeakers"
+// import WhyAttend from "./Components/WhyAttend/WhyAttend"
+// import PastAttendees from "./Components/PastAttendees/PastAttendees"
+// import RegisterNow from "./Components/RegisterNow/RegisterNow"
+// import TicketOptions from "./Components/TicketOptions/TicketOptions"
+// import Connect from "./Components/Connect/Connect"
+// import Footer from "./Components/Footer/Footer"
 
 const Home = () => (
 
@@ -12,7 +19,14 @@ const Home = () => (
     <FirstBlock />
     <Collaboration />
     <Advantages />
-    {/* <Agenda/> */}
+    <Agenda />
+    {/* <KeynoteSpeakers /> */}
+    {/* <WhyAttend /> */}
+    {/* <PastAttendees /> */}
+    {/* <RegisterNow /> */}
+    {/* <TicketOptions /> */}
+    {/* <Connect /> */}
+    {/* <Footer /> */}
   </div>
 )
 
