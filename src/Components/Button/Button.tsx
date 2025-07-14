c11eff14b66a0dced47db9e1a15b95ff51a91cd8
BlcakCobra <valeri.baskayanc.y@gmail.com>
2025-07-14 18:37:36 +0400

    Button Component

---
+++ src/Components/Button/Button.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import upRightArrow from "../../assets/upRightArrow.svg";
+import styles from "./Button.module.scss";
+
+type ButtonProps = {
+    text: string;
+    top: string;
+    left: string;
+    width: string;
+};
+
+const Button: React.FC<ButtonProps> = ({ text, top, left, width }) => (
+    
+    <div
+        className={styles.button}
+        style={{ marginTop: top, marginLeft: left, width: width }}
+    >
+        {text}
+        <img src={upRightArrow} alt="Arrow icon" className={styles.arrowIcon} />
+    </div>
+);
+
+export default Button;
