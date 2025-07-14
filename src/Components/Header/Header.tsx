ee1ff48307476503450c61a4c877aebc6bb41c95
BlcakCobra <valeri.baskayanc.y@gmail.com>
2025-07-14 18:46:38 +0400

    align Header component

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -4,6 +4,7 @@ export default function Header() {
     const navItems = ["Home", "Schedule", "Speakers", "Tickets", "Venue", "Sponsors"]
 
     return (
+
         <header className={styles.header}>
             <div className={styles.logo}>nexus</div>
             <nav className={styles.nav}>
