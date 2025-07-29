1d61380c8ff2b81ad6fccf522612f449e610ef5a
BlcakCobra <valeri.baskayanc.y@gmail.com>
2025-07-29 17:17:46 +0400

    eslint changes

--- src/Components/Title/Title.tsx
+++ src/Components/Title/Title.tsx
@@ -1,17 +1,17 @@
-import type React from "react"
-import styles from "./Titlemodule.module.scss"
+import type React from 'react';
+
+import styles from './Titlemodule.module.scss';
 
 type TitleProps = {
-    title: string,
-    description: string
-}
+  title: string;
+  description: string;
+};
 
 const Title: React.FC<TitleProps> = ({ title, description }) => (
+  <div className={styles.titles}>
+    <h1 className={styles.Title}>{title}</h1>
+    <p className={styles.description}>{description}</p>
+  </div>
+);
 
-    <div className={styles.titles}>
-        <h1 className={styles.Title}>{title}</h1>
-        <p className={styles.description}>{description}</p>
-    </div>
-)
-
-export default Title
+export default Title;
