e116ac2f2b71d0e6227db6497a14fe5c2e91ccfd
BlcakCobra <valeri.baskayanc.y@gmail.com>
2025-07-29 16:36:04 +0400

    eslint changes

--- src/Components/WhyAttend/Reasons/Reasons.tsx
+++ src/Components/WhyAttend/Reasons/Reasons.tsx
@@ -1,46 +1,49 @@
-import styles from "./Reasons.module.scss";
+import styles from './Reasons.module.scss';
 
 const reasonContent = [
-    {
-        title: "Cutting-Edge Insights",
-        description: "Gain firsthand knowledge from top AI experts and pioneers shaping the industry.",
-        num: "01",
-        reverse: false,
-    },
-    {
-        title: "Hands-On Learning",
-        description: "Participate in interactive workshops, live demos, and deep-dive sessions to sharpen your skills.",
-        num: "02",
-        reverse: false,
-    },
-    {
-        title: "Exclusive Networking",
-        description: "Connect with AI leaders, investors, startups, and fellow professionals at curated networking events.",
-        num: "03",
-        reverse: true,
-    },
-    {
-        title: "Innovation Showcase",
-        description: "Explore groundbreaking AI solutions, from emerging startups to tech giants redefining the future.",
-        num: "04",
-        reverse: true,
-    },
+  {
+    title: 'Cutting-Edge Insights',
+    description: 'Gain firsthand knowledge from top AI experts and pioneers shaping the industry.',
+    num: '01',
+    reverse: false,
+  },
+  {
+    title: 'Hands-On Learning',
+    description:
+      'Participate in interactive workshops, live demos, and deep-dive sessions to sharpen your skills.',
+    num: '02',
+    reverse: false,
+  },
+  {
+    title: 'Exclusive Networking',
+    description:
+      'Connect with AI leaders, investors, startups, and fellow professionals at curated networking events.',
+    num: '03',
+    reverse: true,
+  },
+  {
+    title: 'Innovation Showcase',
+    description:
+      'Explore groundbreaking AI solutions, from emerging startups to tech giants redefining the future.',
+    num: '04',
+    reverse: true,
+  },
 ];
 
 const Reasons = () => (
-    <div className={styles.reasons}>
-        {reasonContent.map((el, index) => (
-            <div key={index} className={styles.reasonsContainer}>
-                <div className={`${styles.content} ${el.reverse ? styles.reverse : ""}`}>
-                    <div className={styles.num}>{el.num}</div>
-                    <div className={styles.textBlock}>
-                        <h2 className={styles.title}>{el.title}</h2>
-                        <p className={styles.description}>{el.description}</p>
-                    </div>
-                </div>
-            </div>
-        ))}
-    </div>
+  <div className={styles.reasons}>
+    {reasonContent.map((el, index) => (
+      <div key={index} className={styles.reasonsContainer}>
+        <div className={`${styles.content} ${el.reverse ? styles.reverse : ''}`}>
+          <div className={styles.num}>{el.num}</div>
+          <div className={styles.textBlock}>
+            <h2 className={styles.title}>{el.title}</h2>
+            <p className={styles.description}>{el.description}</p>
+          </div>
+        </div>
+      </div>
+    ))}
+  </div>
 );
 
 export default Reasons;
--- src/Components/WhyAttend/WhyAttend.tsx
+++ src/Components/WhyAttend/WhyAttend.tsx
@@ -1,13 +1,16 @@
-import Title from "../Title/Title";
-import Reasons from "./Reasons/Reasons";
-import styles from "./WhyAttend.module.scss";
+import Title from '../Title/Title';
+
+import Reasons from './Reasons/Reasons';
+import styles from './WhyAttend.module.scss';
 
 const WhyAttend = () => (
-
-    <section className={styles.whyAttend}>
-        <Title title="WHY ATTEND?" description="Discover why Next-Gen AI Summit is the must-attend event for AI professionals, innovators, and industry leaders." />
-        <Reasons />
-    </section>
+  <section className={styles.whyAttend}>
+    <Title
+      title="WHY ATTEND?"
+      description="Discover why Next-Gen AI Summit is the must-attend event for AI professionals, innovators, and industry leaders."
+    />
+    <Reasons />
+  </section>
 );
 
 export default WhyAttend;
